7536e8a33b946633ac78c913855e1eb127a973c2
flyingwr <leekchuucmrc@gmail.com>
2021-03-16 01:51:41 -0300

    add logs

--- server.py
+++ server.py
@@ -70,7 +70,7 @@ class Api:
 				if client_version == self.version:
 					response['success'] = True
 
-					addrr = request.transport.get_extra_info("peername")
+					addrr = request.transport.get_extra_info("peername")[0]
 					if addrr not in self.ips.keys():
 						access_token = generate_token()
 						self.ips[addrr] = (datetime.datetime.now().timestamp(), access_token)
