a8d80391e2ba1679533e5e13d7de0ec3eda065a5
flyingwr <leekchuucmrc@gmail.com>
2021-03-30 01:59:19 -0300

    set download sleep time

--- server.py
+++ server.py
@@ -41,18 +41,20 @@ class Api:
 	async def fetch(self):
 		while True:
 			session = ClientSession()
+			sleep = 0
 
 			try:
 				swf_len = (await session.head("https://www.transformice.com/Transformice.swf")).headers["Content-Length"]
 				if self.last_swf_len != swf_len:
 					await self.parser.start()
 					self.last_swf_len = swf_len
+
+					sleep = 8
 			except Exception:
 				print("Failed to download Transformice SWF")
-			finally:
-				await asyncio.sleep(8)
 
 			await session.close()
+			await asyncio.sleep(sleep)
 		
 	async def update(self):
 		self.pool = await aiomysql.create_pool(host="remotemysql.com",
