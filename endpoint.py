85fe555d92e3120bfa771e2d1cc440e57a2e2821
flyingwr <leekchuucmrc@gmail.com>
2021-04-29 16:47:10 -0300

    Update endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -217,10 +217,9 @@ class Api:
 				key = self.tokens[access_token]["key"]
 
 				level = self.tokens[access_token]["level"]
-				if level == "PLATINUM" or key == "rm69t3p7":
+				limit = 1
+				if level == "PLATINUM":
 					limit = 10
-				else:
-					limit = 3
 				if len(self.tokens[access_token]["ips"]) < limit:
 					if addr not in self.tokens[access_token]["ips"]:
 						self.tokens[access_token]["ips"].append(addr)
