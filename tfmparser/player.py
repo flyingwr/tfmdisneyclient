fa9bca9c3f592a46e77c769a02242c7910466800
flyingwr <leekchuucmrc@gmail.com>
2021-05-01 15:45:17 -0300

    update chargeur

--- tfmparser/player.py
+++ tfmparser/player.py
@@ -1,4 +1,4 @@
-from .regex import GET_PROPERTY, find_one
+from .regex import GET_PROPERTY, SET_PROPERTY, find_one
 from typing import Dict, List
 
 class Player(dict):
@@ -21,4 +21,32 @@ class Player(dict):
 														self["player"] = (await find_one(GET_PROPERTY, dumpscript[line + 6])).group(2)
 														self["is_dead"] = (await find_one(GET_PROPERTY, dumpscript[line + 7])).group(2)
 														break
+		for line, content in enumerate(dumpscript):
+			if 'pushstring "AnimAttaque' in content:
+				setproperty = (await find_one(SET_PROPERTY, dumpscript[line + 1])).group(1)
+				for x in range(len(dumpscript)):
+					if f"getproperty <q>[public]::{setproperty}" in dumpscript[x]:
+						for y in range(x, x + 60):
+							if "scaleX" in dumpscript[y]:
+								for z in range(y, 0, -1):
+									if "getlocal_0" in dumpscript[z] and "getproperty" in dumpscript[z + 1]:
+										self["player_bitmap"] = (await find_one(GET_PROPERTY, dumpscript[z + 1])).group(2)
+										for i in range(len(dumpscript)):
+											if f"getproperty <q>[public]::{self['player_bitmap']}" in dumpscript[i]:
+												if "visible" in dumpscript[i + 3]:
+													for j in range(i + 3, i + 18):
+														if "getslot" in dumpscript[j] and "addChild" in dumpscript[j + 1]:
+															for k in range(j, 0, -1):
+																if "setproperty" in dumpscript[k]:
+																	self["has_image"] = (
+																		await find_one(SET_PROPERTY, dumpscript[k])).group(1)
+																	break
+															break
+													else:
+														continue
+													break
+										break
+								break
+						break
+				break
 		return self
