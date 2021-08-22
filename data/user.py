c4af5715a8f89ead5362915ed8feef5a48b1565e
flyingwr <renanferncmrc@gmail.com>
2021-08-22 15:04:41 -0300

    update mongodb user model

--- data/user.py
+++ data/user.py
@@ -3,9 +3,9 @@ import mongoengine
 
 class User(mongoengine.Document):
     key = mongoengine.StringField(required=True)
-    uuid = mongoengine.UUIDField(binary=False, null=True, required=True)
+    uuid = mongoengine.UUIDField(binary=False, null=True)
     
-    premium_level = mongoengine.StringField(default="SILVER", required=True)
+    premium_level = mongoengine.StringField(default="SILVER")
     
     browser_access = mongoengine.BooleanField(default=True)
     browser_access_token = mongoengine.StringField(null=True)
