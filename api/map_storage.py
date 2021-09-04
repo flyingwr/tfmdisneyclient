94283f93b89fcdda73361e7739fdefbbf19734cf
flyingwr <renanferncmrc@gmail.com>
2021-09-04 01:27:04 -0300

    update api

--- api/map_storage.py
+++ api/map_storage.py
@@ -59,7 +59,7 @@ class MapStorage(web.View):
 			raise web.HTTPUnauthorized()
 
 		data = await self.request.post()
-		map_data = data.get("mapdata")
+		code, info = data.get("code"), data.get("info")
 		
 		method = self.request.query.get("method")
 
@@ -82,31 +82,31 @@ class MapStorage(web.View):
 						infrastructure.loop.create_task(infrastructure.del_lsmap(access_token))
 
 					return web.Response(body=result.encode())
-
-				if map_data:
+				elif method in ("del", "save"):
 					data_decoded = cryptjson.text_decode(found_map_data)
+					_search = re.search(b"%s:([^#]*)" % code.encode(), data_decoded)
 
-					search = map_pattern.search(cryptjson.text_decode(map_data))
-					if search is not None:
-						map_code, info = search.group(1), search.group(2)
-						if map_code in data_decoded:
-							_search = re.search(b"%s:([^#]*)" % map_code, data_decoded)
-							if _search:
-								if method == "save":
-									data_decoded = data_decoded.replace(
-										_search.group(),
-										b"%s:%s" % (map_code, info)
-									)
-								elif method == "del":
-									data_decoded = data_decoded.replace(_search.group(), b"")
+					if method == "save":
+						if info:
+							if code.encode() in data_decoded:
+								if _search:
+									if method == "save":
+										data_decoded = data_decoded.replace(
+											_search.group(),
+											b"%s:%s" % (code.encode(), info.encode())
+										)
+								else:
+									data_decoded += b"#%s:%s" % (code.encode(), info.encode())
 						else:
-							if method == "save":
-								data_decoded += b"#%s:%s" % (map_code, info)
+							raise web.HTTPBadRequest()
+					elif method == "del":
+						if _search:
+							data_decoded = data_decoded.replace(_search.group(), b"")
 
-						_map.update(data=cryptjson.text_encode(map_pattern2.sub(
-							b"", data_decoded.replace(b"##", b"#"))))
+					_map.update(data=cryptjson.text_encode(map_pattern2.sub(
+						b"", data_decoded.replace(b"##", b"#"))))
 
-						raise web.HTTPNoContent()
+					raise web.HTTPNoContent()
 
 				raise web.HTTPBadRequest()
 
