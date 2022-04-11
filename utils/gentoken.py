06a5e674a3f1a18bf5ce3a166a880254b9402bec
Renan <renanferncmrc@gmail.com>
2022-04-11 00:40:47 -0300

    update utils

--- utils/gentoken.py
+++ utils/gentoken.py
@@ -1,21 +1,10 @@
-from hmac import digest
-from typing import Optional
+import secrets
 
-import hashlib
-import uuid
-import base64
+def gen_token() -> str:
+	return secrets.token_urlsafe(32)
 
-def gen_token(b64: Optional[bool] = False) -> str:
-	sha256 = hashlib.sha256(uuid.uuid4().bytes)
-	if b64:
-		return base64.b64encode(sha256.digest()).decode()
-	return sha256.hexdigest()
-
-def gen_browser_token(b64: Optional[bool] = False) -> str:
-	_uuid = uuid.uuid4()
-	if b64:
-		return base64.b64encode(_uuid.bytes).decode()
-	return str(_uuid)
+def gen_browser_token() -> str:
+	secrets.token_hex(16)
 
 if __name__ == "__main__":
 	print(gen_token())
