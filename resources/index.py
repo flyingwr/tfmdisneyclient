1552afa8153eff41b23fc17dc787e208c2a34c4f
flyingwr <renanferncmrc@gmail.com>
2022-06-20 15:57:52 -0300

    update index page request handler

--- resources/index.py
+++ resources/index.py
@@ -5,7 +5,7 @@ async def index(request: web.Request):
     response = web.FileResponse("./public/index/index.html")
 
     cookies = request.cookies
-    if not (browser_access_token := cookies.get("browser_access_token")) or len(browser_access_token) != 32:
+    if not (browser_access_token := cookies.get("browser_access_token")) or browser_access_token == "None":
         response.set_cookie("browser_access_token", gen_browser_token(), max_age=365 * 24 * 60 * 60)
 
     return response
